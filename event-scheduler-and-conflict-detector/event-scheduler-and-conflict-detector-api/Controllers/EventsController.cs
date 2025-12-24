d7faad9f83a07d6ac0f6f9d205405340a0b5af9f
tijes <tijes@DESKTOP-D6TFKBH>
2025-12-24 13:52:15 +0100

    Create Event Class inside a Models Folder

---
+++ event-scheduler-and-conflict-detector/event-scheduler-and-conflict-detector-api/Controllers/EventsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace event_scheduler_and_conflict_detector_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+    }
+}
---
+++ event-scheduler-and-conflict-detector/event-scheduler-and-conflict-detector-api/Models/Event.cs
@@ -0,0 +1,37 @@
+﻿namespace event_scheduler_and_conflict_detector_api.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+		public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Location { get; set; }
+
+        public List<String> Attendees { get; set; }
+
+        public enum EventType { get; set; }
+
+        public Event()
+        {
+            
+        }
+		public Event(Guid id, string title, string description, DateTime startTime, DateTime endTime, string location, List<string> attendees)
+		{
+			Id = id;
+			Title = title;
+			Description = description;
+			StartTime = startTime;
+			EndTime = endTime;
+			Location = location;
+			Attendees = attendees;
+		}
+
+	}
+}
