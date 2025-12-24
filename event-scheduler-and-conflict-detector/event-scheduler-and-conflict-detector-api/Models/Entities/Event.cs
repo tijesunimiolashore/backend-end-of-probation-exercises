98b5445040a546ce55dc26ed34edcc99395a577d
tijes <tijes@DESKTOP-D6TFKBH>
2025-12-24 14:23:58 +0100

    Created a Model Folder to store the Event Class

---
+++ event-scheduler-and-conflict-detector/event-scheduler-and-conflict-detector-api/Models/Entities/Event.cs
@@ -0,0 +1,41 @@
+﻿namespace event_scheduler_and_conflict_detector_api.Models.Entities
+{
+    public class Event
+    {
+			public Guid Id { get; set; } = Guid.NewGuid();
+
+			public string Title { get; set; }
+
+			public string Description { get; set; }
+
+			public DateTime StartTime { get; set; }
+
+			public DateTime EndTime { get; set; }
+
+			public string Location { get; set; }
+
+			public List<String> Attendees { get; set; }
+
+			public Enum EventType { get; set; }
+
+			public Event()
+			{
+
+			}
+			public Event(Guid id, string title, string description, DateTime startTime, DateTime endTime, string location, List<string> attendees, Enum eventType)
+			{
+				Id = id;
+				Title = title;
+				Description = description;
+				StartTime = startTime;
+				EndTime = endTime;
+				Location = location;
+				Attendees = attendees;
+				EventType = eventType;
+		}
+
+		}
+	}
+
+}
+}
---
+++ event-scheduler-and-conflict-detector/event-scheduler-and-conflict-detector-api/Models/Entities/EventType.cs
@@ -0,0 +1,9 @@
+﻿namespace event_scheduler_and_conflict_detector_api.Models.Entities
+{
+    public enum EventType { 
+		 Meeting,
+         Appointment,
+         Reminder 
+
+}
+}
