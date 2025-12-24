e48376ba3bc8c56421c44589920f2bf2fdb6426d
tijes <tijes@DESKTOP-D6TFKBH>
2025-12-24 14:37:09 +0100

    Add Data Transfer Object for Adding New Events

---
+++ event-scheduler-and-conflict-detector/event-scheduler-and-conflict-detector-api/Models/AddEventDto.cs
@@ -0,0 +1,19 @@
+﻿namespace event_scheduler_and_conflict_detector_api.Models
+{
+    public class AddEventDto
+    {
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public DateTime StartTime { get; set; }
+
+		public DateTime EndTime { get; set; }
+
+		public string Location { get; set; }
+
+		public List<String> Attendees { get; set; }
+
+		public Enum EventType { get; set; }
+	}
+}
