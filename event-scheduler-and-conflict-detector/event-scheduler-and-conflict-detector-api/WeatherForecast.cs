dae2232eda89b181b4210e0729f7cc62b0d58d34
tijes <tijes@DESKTOP-D6TFKBH>
2025-12-24 13:03:23 +0100

    First Commit

---
+++ event-scheduler-and-conflict-detector/event-scheduler-and-conflict-detector-api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace event_scheduler_and_conflict_detector_api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
